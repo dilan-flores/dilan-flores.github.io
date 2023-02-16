1854dd9d3bc8b7fc59b33ad79788441136594f3d
Dilan <117755180+dilan-flores@users.noreply.github.com>
2023-02-16 16:12:37 -0500

    Incorporación de DOM

---
+++ app.js
@@ -0,0 +1,22 @@
+let nav = document.getElementById("nav");
+nav.style.textAlign = "center";
+nav.style.color = "darkslategray";
+
+let contacto = document.getElementById("con");
+contacto.style.color = "darkred";
+
+let r1 = document.getElementById("reg1");
+r1.style.color = "crimson";
+
+let r2 = document.getElementById("reg2");
+r2.style.color = "crimson";
+
+let r3 = document.getElementById("reg3");
+r3.style.color = "crimson";
+
+let r4 = document.getElementById("reg4");
+r4.style.color = "crimson";
+
+
+
+
